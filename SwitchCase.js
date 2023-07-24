d4b5447bf1292fb2f672f91b99b2c6b4bdac40a2
DavidTD789 <138784445+DavidTD789@users.noreply.github.com>
2023-07-24 13:35:28 +0400

    SwitchCase

---
+++ SwitchCase.js
@@ -0,0 +1,26 @@
+let n = prompt('Enter week day - ')
+switch(n){
+    case '1':
+        console.log('erkushabti')
+        break
+    case '2':
+        console.log('ereqshabti')
+        break
+    case '3':
+        console.log('choreqshabti')
+        break
+    case '4':
+        console.log('hingshabti')
+        break
+    case '5':
+        console.log('urbat')
+        break
+    case '6':
+        console.log('shabat')
+        break
+    case '7':
+        console.log('kiraki')
+        break
+    default:
+        console.log('error')
+}
